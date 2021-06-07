ceae8cae6fc5d95ee27a8676880e0da23bce9ac0
brb22 <brb22@aber.ac.uk>
2021-06-07 16:28:32 +0000

    Added indicators: SMA, EMA

---
+++ ema.py
@@ -0,0 +1,28 @@
+def ema(close, period=10):
+    """Indicator: Exponential Moving Average (EMA)"""
+
+    # Validate params
+    period = int(period) if period > 1 else 10
+
+    return close.ewm(span=period, adjust=False).mean()
+
+
+    """
+    An exponential moving average (EMA) is a type of moving average (MA) 
+    that places a greater weight and significance on the most recent data 
+    points.
+
+    Sources:
+        https://www.investopedia.com/terms/e/ema.asp
+
+    Calculation:
+        close.ewm(span=period, adjust=False).mean()
+
+    Params:
+        close (Pandas Series) Series of closing prices
+        period (int): The period. Default 10
+
+
+    Returns:
+        Pandas Series
+    """
---
+++ sma.py
@@ -0,0 +1,28 @@
+def sma(close, period=10):
+    """Indicator: Simple Moving Average (SMA)"""
+
+    # Validate params
+    period = int(period) if period > 1 else 10
+
+    return close.rolling(period).mean()
+
+
+    """
+    A simple moving average (SMA) calculates the average of a selected 
+    range of prices, usually closing prices, by the number of periods in 
+    that range.
+
+    Sources:
+        https://www.investopedia.com/terms/s/sma.asp
+
+    Calculation:
+        close.rolling(period).mean()
+
+    Params:
+        close (Pandas Series) Series of closing prices
+        period (int): The period. Default 10
+
+
+    Returns:
+        Pandas Series
+    """
